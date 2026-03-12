8187f7639644a7a94f0b320d6a8903b4c1028d00
Dyageti.Sai Sreeman <260058045+sreeman-dyageti@users.noreply.github.com>
2026-03-12 07:45:19 +0530

    fix: env variables + start script

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.set("view engine", "ejs");
 
 // DB connection
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "bookshelf",
-  password: "sreeman@db",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
@@ -178,20 +178,6 @@ app.post("/delete/:id",async (req,res)=>{
   }
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
